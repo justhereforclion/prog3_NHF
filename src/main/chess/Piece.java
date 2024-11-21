60a21f9b306d25a13211dd8f989b1182b8e0bab1
Nádudvari Örs Máté <orsnadudvari@gmail.com>
2024-11-21 14:25:53 +0100

    Add functionality for keeping track whether a piece has moved or not

--- src/main/chess/Piece.java
+++ src/main/chess/Piece.java
@@ -10,15 +10,21 @@ public class Piece {
     
     private final Color color;
     private final PieceType type;
+    private boolean hasMoved;
 
     public Piece(Color c, PieceType t){
         this.color = c;
         this.type = t;
+        this.hasMoved = false;
     }
 
     public Color getColor(){return this.color;}
 
     public PieceType getType(){return this.type;}
+
+    public boolean hasMoved(){return this.hasMoved;}
+
+    public void setHasMoved(boolean b){this.hasMoved = b;}
 
 
 }
